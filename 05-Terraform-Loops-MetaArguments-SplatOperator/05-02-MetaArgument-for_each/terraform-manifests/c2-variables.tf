28b244eb6d28a694c6e564430dad75d1b2100034
Kaveh Saleki <kaveh.saleki@hotmail.com>
2022-10-15 08:30:10 +0200

    exercise 5-2 modified

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c2-variables.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c2-variables.tf
@@ -3,14 +3,14 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  default = "us-east-1"
+  default = "eu-central-1"
 }
 
 # AWS EC2 Instance Type
 variable "instance_type" {
   description = "EC2 Instnace Type"
   type = string
-  default = "t3.micro"
+  default = "t2.micro"
 }
 
 # AWS EC2 Instance Key Pair
@@ -20,4 +20,20 @@ variable "instance_keypair" {
   default = "terraform-key"
 }
 
+# AWS EC2 Instance Type - List
+variable "instance_type_list" {
+  description = "EC2 Instance Type"
+  type = list(string)
+  default = ["t2.micro", "t2.small", "t2.large"]  
+}
 
+# AWS EC2 Instance Type - Map
+variable "instance_type_map" {
+  description = "EC2 Instance Type"
+  type = map(string)
+  default = {
+    "dev" = "t2.micro"
+    "qa" = "t2.small"
+    "prod" = "t2.large"
+  }
+}

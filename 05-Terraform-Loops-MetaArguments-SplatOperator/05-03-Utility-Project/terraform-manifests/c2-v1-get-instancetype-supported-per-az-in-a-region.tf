3170fbc6e6e18eec6811ccc64df1408809083ac6
Kaveh Saleki <kaveh.saleki@hotmail.com>
2022-10-15 08:47:24 +0200

    exercise 5-3 modified

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
@@ -6,8 +6,8 @@ data "aws_ec2_instance_type_offerings" "my_ins_type1" {
   }
   filter {
     name   = "location"
-    #values = ["us-east-1a"]
-    values = ["us-east-1e"]
+    #values = ["eu-central-1a"]
+    values = ["eu-central-1c"]
   }
   location_type = "availability-zone"
 }
--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
@@ -2,7 +2,7 @@
 # Get the List of Availability Zones in a Particular region where that respective Instance Type is supported
 # Datasource
 data "aws_ec2_instance_type_offerings" "my_ins_type2" {
-  for_each = toset([ "us-east-1a", "us-east-1b", "us-east-1e" ])
+  for_each = toset([ "eu-central-1a", "eu-central-1b", "eu-central-1d" ])
   filter {
     name   = "instance-type"
     values = ["t3.micro"]
